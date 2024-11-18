48d7f3f9de8edc3823e3423dda2967eb4184017e
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-11-18 11:04:52 +0200

    Changed log-in layout styling

--- Application/resources/views/components/text-input.blade.php
+++ Application/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'mb-5 p-3 w-80 focus:border-blue-700 rounded border-2 outline-none']) }}>
--- Application/resources/views/layouts/guest.blade.php
+++ Application/resources/views/layouts/guest.blade.php
@@ -14,16 +14,14 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
+        <div class="w-screen h-screen flex justify-center items-center bg-gray-100">
+            <div class="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md">
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-20 h-20 text-gray-600 mb-2" viewbox="0 0 20 20" fill="currentColor">
+                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                </svg>
+               {{ $slot }}
             </div>
         </div>
     </body>
