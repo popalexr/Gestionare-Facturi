13650678156d69cc09a26ff95833322caf354437
popalexr <alexpop8@hotmail.com>
2024-11-18 11:41:02 +0200

    Update user migration to set default status as inactive and add reverse migration.

--- Application/database/migrations/2024_11_18_091213_add_user_columns.php
+++ Application/database/migrations/2024_11_18_091213_add_user_columns.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('phone')->nullable()->after('email_verified_at');
             $table->string('avatar')->nullable()->after('phone');
-            $table->string('status')->enum(['active', 'inactive'])->default('active')->after('avatar');
+            $table->string('status')->enum(['active', 'inactive'])->default('inactive')->after('avatar');
             $table->string('role')->default('user')->after('status');
             $table->softDeletes();
         });
@@ -25,6 +25,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('avatar');
+            $table->dropColumn('status');
+            $table->dropColumn('role');
+            $table->dropSoftDeletes();
+        });
     }
 };
