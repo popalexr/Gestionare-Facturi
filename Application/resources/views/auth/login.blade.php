9b9c53673de3fd4a5389713a672e188ccc19325e
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-11-18 11:11:00 +0200

    Fixed justify-between login button

--- Application/resources/views/auth/login.blade.php
+++ Application/resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-bet mt-4 ">
+        <div class="flex items-center justify-between mt-4 ">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-blue-700 hover:text-gray-900 dark:hover:text-blue-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
