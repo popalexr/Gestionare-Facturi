60a84d9d873315431f5017c6b7f08c4f899fe4a0
popalexr <alexpop8@hotmail.com>
2024-11-18 11:19:26 +0200

    Added users column migration.

---
+++ Application/database/migrations/2024_11_18_091213_add_user_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->nullable()->after('email_verified_at');
+            $table->string('avatar')->nullable()->after('phone');
+            $table->string('status')->enum(['active', 'inactive'])->default('active')->after('avatar');
+            $table->string('role')->default('user')->after('status');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
