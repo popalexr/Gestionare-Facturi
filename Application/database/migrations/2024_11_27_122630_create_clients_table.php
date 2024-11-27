bf1377ce0287e6a3de10e632f9c9ca502d9ee0bf
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-11-27 14:40:55 +0200

    Added Client model and migration

---
+++ Application/database/migrations/2024_11_27_122630_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('client_type', ['individual', 'company']);
+            $table->string('country');
+            $table->string('county');
+
+            $table->string('city');
+            $table->string('address');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
