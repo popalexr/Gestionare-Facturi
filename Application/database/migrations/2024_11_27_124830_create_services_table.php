cd29d7ef15708840e8a5fe88369b5472acfa626b
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-11-27 21:10:30 +0200

    Added Services migration and controller

---
+++ Application/database/migrations/2024_11_27_124830_create_services_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->id('vat_id')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->enum('currency', ['ron', 'eur', 'usd']);
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
