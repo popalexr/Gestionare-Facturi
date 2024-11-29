5c0cbe5611a7a61eb889e0e5b35145a4d097c103
popalexr <alexpop8@hotmail.com>
2024-11-29 10:20:06 +0200

    Update services table migration to use unsigned big integer for vat_id

--- Application/database/migrations/2024_11_27_124830_create_services_table.php
+++ Application/database/migrations/2024_11_27_124830_create_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->id('vat_id')->nullable();
+            $table->unsignedBigInteger('vat_id')->nullable();
             $table->decimal('price', 10, 2);
             $table->enum('currency', ['ron', 'eur', 'usd']);
             $table->string('description')->nullable();
