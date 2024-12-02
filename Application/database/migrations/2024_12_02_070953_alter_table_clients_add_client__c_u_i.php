a5cd1ab7d6b4e9ef991848a19df68ed9ad86d2bb
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-12-02 10:10:49 +0200

    Update migration to add CUI field after name in clients table

--- Application/database/migrations/2024_12_02_070953_alter_table_clients_add_client__c_u_i.php
+++ Application/database/migrations/2024_12_02_070953_alter_table_clients_add_client__c_u_i.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('clients', function (Blueprint $table) {
-            $table->string('cui')->nullable()->after('email');
+            $table->string('cui')->nullable()->after('name');
         });
     }
 
