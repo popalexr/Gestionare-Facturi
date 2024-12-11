1729b3a8e9cdac7810dd65beb51a9598bc48a93b
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-12-11 14:28:51 +0200

    Add form validation rules and messages to ClientsFormController

--- Application/app/Http/Controllers/Clients/ClientsFormController.php
+++ Application/app/Http/Controllers/Clients/ClientsFormController.php
@@ -27,4 +27,36 @@ class ClientsFormController extends Controller
     {
         return redirect()->route('clients.index');
     }
+
+    private function getFormValidationRules() : Array
+    {
+        return [
+            'name' => 'required|string|max:255', 
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'county' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'client_type' => 'required|in:individual,company',
+            
+        ];
+    }
+
+    private function getFormValidationMessages() : Array
+    {
+        return [
+            'name.required' => 'Name field is required.',
+            'name' => 'Name field is invalid.',
+            'adress.required' => 'Address field is required.',
+            'adress' => 'Address field is invalid.',
+            'city.required' => 'City field is required.',
+            'city' => 'City field is invalid.',
+            'county.required' => 'County field is required.',
+            'county' => 'County field is invalid.',
+            'country.required' => 'Country field is required.',
+            'country' => 'Country field is invalid.',
+            'client_type.required' => 'Client type field is required.',
+            'client_type.in' => 'Client type field is invalid.',
+        ];
+    }
+
 }
