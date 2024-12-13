d3c26f06ab87ec76d0edbd786d6b58f503a05569
popalexr <alexpop8@hotmail.com>
2024-12-13 10:31:35 +0200

    Refactor ServicesFormController.

--- Application/app/Http/Controllers/Services/ServicesFormController.php
+++ Application/app/Http/Controllers/Services/ServicesFormController.php
@@ -14,39 +14,42 @@ class ServicesFormController extends Controller
 
     public function __invoke(Request $request) : View
     {
+        $this->id = $request->query('id');
+
         if($this->id == 0)
             return view('services.add-services');
+        
         return view('services.services-form')->with([
             'id', $this->id
         ]);
     }
+
     public function post(Request $request) : RedirectResponse
     {
         return redirect()->route('services.index');
     }
 
-
     private function getFormValidationRules() : Array
     {
         return [
-            'name' => 'required|string|max:255',
+            'name'        => 'required|string|max:255',
             'description' => 'required|string|max:255',
-            'price' => 'required|numeric',
-            'currency' => 'required|string|max:255',
+            'price'       => 'required|numeric',
+            'currency'    => 'required|string|max:255',
         ];
     }
 
     private function getFormValidationMessages() : Array
     {
         return [
-            'name.required' => 'Name field is required.',
-            'name' => 'Name field is invalid.',
-            'description.required' => 'Description field is required.',
-            'description' => 'Description field is invalid.',
-            'price.required' => 'Price field is required.',
-            'price' => 'Price field is invalid.',
-            'currency.required' => 'Currency field is required.',
-            'currency' => 'Currency field is invalid.',
+            'name.required'         => 'Name field is required.',
+            'name'                  => 'Name field is invalid.',
+            'description.required'  => 'Description field is required.',
+            'description'           => 'Description field is invalid.',
+            'price.required'        => 'Price field is required.',
+            'price'                 => 'Price field is invalid.',
+            'currency.required'     => 'Currency field is required.',
+            'currency'              => 'Currency field is invalid.',
         ];
     }
 }   
