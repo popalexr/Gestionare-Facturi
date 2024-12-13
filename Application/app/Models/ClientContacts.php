6f9ae58de0296d67e334a51a69475eb33bc26246
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-12-13 08:43:47 +0200

    Add ClientContacts model and migration for clients_contacts table.

---
+++ Application/app/Models/ClientContacts.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientContacts extends Model
+{
+    protected $table = 'client_contacts';
+}
---
+++ Application/database/migrations/2024_12_13_062134_add_clients_contacts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
