55a72f34137d9628cb704594a0dfd11ac755d3be
Nistor Dorin <nistor.dorin.gabriel@gmail.com>
2024-12-16 09:12:50 +0200

    Add fields for city, country, county, and client type in client addition form

--- Application/resources/views/clients/add-clients.blade.php
+++ Application/resources/views/clients/add-clients.blade.php
@@ -63,6 +63,27 @@
                                         <input type="text" name="address" id="address"
                                             class="mt-2 w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:text-gray-300">
                                     </div>
+                                    <div>
+                                        <label for="city" class="block text-sm font-medium text-gray-700 dark:text-gray-200 text-center">City</label>
+                                        <input type="text" name="city" id="city"
+                                            class="mt-2 w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:text-gray-300">
+                                    </div>
+                                    <div>
+                                        <label for="country" class="block text-sm font-medium text-gray-700 dark:text-gray-200 text-center">Country</label>
+                                        <input type="text" name="country" id="country"
+                                            class="mt-2 w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:text-gray-300">
+                                    </div>
+                                    <div>
+                                        <label for="county" class="block text-sm font-medium text-gray-700 dark:text-gray-200 text-center">County</label>
+                                        <input type="text" name="county" id="county"
+                                            class="mt-2 w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:text-gray-300">
+                                    </div>
+                                    <div>
+                                        <select name="client_type" id="client_type">
+                                            <option value="individual">Individual</option>
+                                            <option value="company">Company</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
 
