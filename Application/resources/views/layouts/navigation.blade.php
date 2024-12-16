443529ca119ad301063dc13230139090530ac1ad
Pop Cristian <you@example.com>
2024-12-16 08:47:02 +0200

    adding Notification Bar

--- Application/resources/views/layouts/navigation.blade.php
+++ Application/resources/views/layouts/navigation.blade.php
@@ -113,12 +113,29 @@
           
           <!-- Right Section -->
           <div class="flex items-center space-x-4">
-                <!-- Notifications -->
-                <button class="p-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 2a6 6 0 00-6 6v3.586L3.293 13.293A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
-                    </svg>
-                </button>
+            <!-- Notifications Dropdown -->
+            <div x-data="{ notificationsOpen: false, notifications: ['There are no notifications at the moment.'] }" class="relative">
+              <button @click="notificationsOpen = !notificationsOpen" class="flex items-center text-sm p-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 2a6 6 0 00-6 6v3.586L3.293 13.293A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
+                </svg>
+              </button>
+
+              <!-- Notifications Drop-down Menu -->
+              <div x-show="notificationsOpen" @click.outside="notificationsOpen = false" x-transition
+                class="absolute right-0 z-50 mt-2 w-48 bg-white text-gray-900 rounded shadow-lg dark:bg-gray-700 dark:text-gray-200">
+                <ul class="py-2">
+                  <template x-for="notification in notifications" :key="notification">
+                    <li class="px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 rounded-md">
+                      <span x-text="notification"></span>
+                    </li>
+                  </template>
+                </ul>
+              </div>
+            </div>
+
+
+
 
               <!-- Profile Dropdown -->
               <div x-data="{ profileDropdownOpen: false }" class="relative">
